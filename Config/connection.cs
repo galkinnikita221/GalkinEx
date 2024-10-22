38cbfd3999ab1cc2db43dc8599359982dab177e2
Vladislav <kobelevadara210@gmail.com>
2024-10-22 10:47:13 +0500

    Изменёно подключение и создан контекст для всех моделей. Galkin

--- Config/connection.cs
+++ Config/connection.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
 
 namespace GalkinEx.Config
 {
     public class Connection
     {
-        public static readonly string config = "Server=127.0.0.1;" + "Trusted_Connection=False;" + "Database=ISP-21-2-5;" + "User=root;" + "Port=3307;";
+        public static readonly string config = "Server=student.permaviat.ru;" + "Trusted_Connection=False;" + "Database=ISP_21_2_2;" + "User=ISP_21_2_2;" + "Port=3306;";
+        public static readonly MySqlServerVersion mySqlServerVersion = new MySqlServerVersion(new Version(8,0,11));
     }
 }
---
+++ Context/AllContexts.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GalkinEx.Models;
+
+namespace GalkinEx.Context
+{
+    public class AllContexts : DbContext
+    {
+        public DbSet<employees> employees { get; set; }
+        public DbSet<material> material { get; set; }
+        public DbSet<materialType> materialType { get; set; }
+        public DbSet<orders> orders { get; set; }
+        public DbSet<partners> partners { get; set; }
+        public DbSet<partnersProducts> partnersProducts { get; set; }
+        public DbSet<product> product { get; set; }
+        public DbSet<production> production { get; set; }
+        public DbSet<productType> productType { get; set; }
+
+        public AllContexts()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.Connection.config, Config.Connection.mySqlServerVersion);
+        }
+    }
+}
