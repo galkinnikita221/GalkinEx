ba7e80e3c32f569188d25ddf0eefb5875e0f1691
Vladislav <kobelevadara210@gmail.com>
2024-10-22 10:01:33 +0500

    Создано подключение. Galkin

---
+++ Config/connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalkinEx.Config
+{
+    public class Connection
+    {
+        public static readonly string config = "Server=127.0.0.1;" + "Trusted_Connection=False;" + "Database=ISP-21-2-5;" + "User=root;" + "Port=3307;";
+    }
+}
